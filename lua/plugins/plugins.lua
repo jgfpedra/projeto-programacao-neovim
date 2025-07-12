8cdbaa77b25d4a05b8375582ce9b6599faaf28d9
jgfpedra <j248349@dac.unicamp.br>
2025-07-12 18:04:32 -0300

    feat: plugins lsp

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -38,28 +38,12 @@ return {
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Common on_attach function (keymaps)
-      local on_attach = function(_, bufnr)
-        local map = function(mode, lhs, rhs)
-          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr })
-        end
-
-        map("n", "gd", vim.lsp.buf.definition)
-        map("n", "K", vim.lsp.buf.hover)
-        map("n", "gr", vim.lsp.buf.references)
-        map("n", "<leader>rn", vim.lsp.buf.rename)
-        map("n", "<leader>ca", vim.lsp.buf.code_action)
-        map("n", "[d", vim.diagnostic.goto_prev)
-        map("n", "]d", vim.diagnostic.goto_next)
-      end
-
       -- Set up servers manually or wait for Mason hook
       local servers = { "lua_ls", }
 
       for _, server in ipairs(servers) do
         lspconfig[server].setup({
           capabilities = capabilities,
-          on_attach = on_attach,
           settings = server == "lua_ls" and {
             Lua = {
               diagnostics = {
@@ -79,7 +63,15 @@ return {
     config = function()
       require("mason").setup()
       require("mason-lspconfig").setup {
-        ensure_installed = { "lua_ls", },
+        ensure_installed = { "lua_ls", 
+        "pylsp",
+        "postgres_lsp",
+        "nginx_language_server",
+        "html",
+        "docker_compose_language_service",
+        "clangd",
+        "ts_ls",
+        },
       }
     end,
   }
